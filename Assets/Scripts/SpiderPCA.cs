dd7d6e17081abfc5d8296e327c2713e4ddbc128c
bluegreenbdc2 <98752090+bluegreenbdc2@users.noreply.github.com>
2023-04-20 15:33:58 +0100

    move to target after max distance met

--- Assets/Scripts/SpiderPCA.cs
+++ Assets/Scripts/SpiderPCA.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class SpiderPCA : MonoBehaviour
 {
     public Transform[] legs;
+    public float maxDistanceFromTarget;
     private int noLegs;
     private Vector3[] localTargetPos;
     private Vector3[] legsPos;
     // Start is called before the first frame update
     void Start()
     {
-    
+        maxDistanceFromTarget = 1f;
         noLegs = legs.Length;
         localTargetPos = new Vector3[noLegs]; 
         legsPos = new Vector3[noLegs]; 
@@ -33,6 +34,7 @@ public class SpiderPCA : MonoBehaviour
         
     }
     
+    Vector3 surfaceNormal;
     void FixedUpdate()
     {
         //print(noLegs);
@@ -53,14 +55,28 @@ public class SpiderPCA : MonoBehaviour
             //Gizmos.DrawCube(transform.TransformPoint(localTargetPos[i]), new Vector3(0.2f, 0.2f, 0.2f));
             
             //raycast down from Max leg step height?
-            if (Physics.Raycast( transform.TransformPoint(localTargetPos[i] + transform.up ) , -transform.up, out RaycastHit hit, 1f))
+            if (surfaceNormal == Vector3.zero) surfaceNormal = -transform.up;
+            if (Physics.Raycast( transform.TransformPoint(localTargetPos[i]) + transform.up , -transform.up, out RaycastHit hit, 2f))
             {
-                Debug.DrawRay(transform.TransformPoint(localTargetPos[i] + transform.up), -transform.up, Color.green);
-                Vector3 normal = hit.normal;
+                Debug.DrawRay(transform.TransformPoint(localTargetPos[i]) + transform.up, -transform.up, Color.green);
+                surfaceNormal = hit.normal;
                 Vector3 hitPoint = hit.point;
-                localTargetPos[i] = transform.InverseTransformPoint(hitPoint);
+                Vector3 newPos = transform.InverseTransformPoint(hitPoint);
+                //if ((newPos - localTargetPos[i]).magnitude > 0.3f)
+                //{
+                    localTargetPos[i] = transform.InverseTransformPoint(hitPoint);
+               //}
+                
                 
             }
+            
+            //get distance between current leg pos and target point
+            if ( (legs[i].position - transform.TransformPoint(localTargetPos[i])).magnitude > maxDistanceFromTarget)
+            {
+                legs[i].position = transform.TransformPoint(localTargetPos[i]);
+                legsPos[i] = legs[i].position;
+            }
+            
         }
     }
     
@@ -73,8 +89,10 @@ public class SpiderPCA : MonoBehaviour
             //legMoving[i] = false;
             
             Gizmos.color = new Color(1, 0, 0, 0.5f);
-            Gizmos.DrawCube(transform.TransformPoint(localTargetPos[i]), new Vector3(0.2f, 0.2f, 0.2f));
-            
+            Gizmos.DrawWireSphere(transform.TransformPoint(localTargetPos[i]), maxDistanceFromTarget);
+            Gizmos.color = new Color(1, 1, 0, 0.5f);
+            Gizmos.DrawCube(transform.TransformPoint(localTargetPos[i] + transform.up), new Vector3(0.2f, 0.2f, 0.2f));
+
         }
     }
 }
