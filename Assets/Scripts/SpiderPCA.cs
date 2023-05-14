b663f4d09bb57e54e3784c0da8ed9db3d2d59a15
bluegreenbdc2 <98752090+bluegreenbdc2@users.noreply.github.com>
2023-05-14 18:46:20 +0100

    better legs once again
    new raycast system

--- Assets/Scripts/SpiderPCA.cs
+++ Assets/Scripts/SpiderPCA.cs
@@ -8,8 +8,8 @@ public class SpiderPCA : MonoBehaviour
     public float legStepHeight = 0.2f;
     public float stepSpeed = 8f;
     public Transform[] legs;
-    public float maxDistanceFromTarget;
-    public float maxTargetSurfaceOffset;
+    public float maxDistanceFromTarget = 1f;
+    public float maxTargetSurfaceOffset = 1f;
     private int noLegs;
     private Vector3[] defaultLocalPos;
     private Vector3[] localTargetPos;
@@ -25,8 +25,8 @@ public class SpiderPCA : MonoBehaviour
     {
         lastPos = transform.position;
         lastBodyUp = transform.up;
-        maxDistanceFromTarget = 1f;
-        maxTargetSurfaceOffset = 1f;
+        //maxDistanceFromTarget = 1f;
+        //maxTargetSurfaceOffset = 1f;
         noLegs = legs.Length;
         defaultLocalPos = new Vector3[noLegs];
         localTargetPos = new Vector3[noLegs]; 
@@ -79,14 +79,65 @@ public class SpiderPCA : MonoBehaviour
             
             //raycast down from Max leg step height?
             if (surfaceNormal == Vector3.zero) surfaceNormal = -transform.up;
-            
+
+            Vector3 aboveSpiderToLegTarget = transform.TransformPoint(defaultLocalPos[i]) - (transform.position + (transform.up * 1.5f));
+            Vector3 LegTargetToBelowSpider = (transform.position - (transform.up * 1.5f)) - transform.TransformPoint(defaultLocalPos[i]);
+            Collider selfCollider = transform.parent.GetComponent<Collider>();
+            selfCollider.enabled = false;
+
+            if (Physics.Raycast((transform.position + transform.up), aboveSpiderToLegTarget, out RaycastHit hit, 5f))
+            {
+
+                Debug.DrawRay((transform.position + transform.up), aboveSpiderToLegTarget, Color.green);
+                Vector3 hitPoint = hit.point;
+                //Debug.DrawRay(hitPoint, transform.up, Color.white);
+                Vector3 newPos = transform.InverseTransformPoint(hitPoint);
+
+                if ((defaultLocalPos[i] - newPos).magnitude < maxTargetSurfaceOffset)
+                {
+                    Debug.Log("real!!!! wow");
+                    localTargetPos[i] = newPos;
+                    //}
+                }
+
+
+            }
+            else
+            {
+                
+                if (Physics.Raycast(transform.TransformPoint(defaultLocalPos[i]), LegTargetToBelowSpider, out RaycastHit bhit, 5f))
+                {
+                    Debug.DrawRay(transform.TransformPoint(defaultLocalPos[i]), LegTargetToBelowSpider, Color.green);
+                    //Debug.DrawRay((transform.position + transform.up), aboveSpiderToLegTarget, Color.green);
+                    Vector3 hitPoint = bhit.point;
+                    //Debug.DrawRay(hitPoint, transform.up, Color.white);
+                    Vector3 newPos = transform.InverseTransformPoint(hitPoint);
+
+                    if ((defaultLocalPos[i] - newPos).magnitude < maxTargetSurfaceOffset)
+                    {
+                        Debug.Log("real!!!! wow");
+                        localTargetPos[i] = newPos;
+                        //}
+                    }
+
+
+                }
+                else
+                {
+                    Debug.DrawRay(transform.TransformPoint(defaultLocalPos[i]), LegTargetToBelowSpider, Color.red);
+                }
+            }
+
+            selfCollider.enabled = true;
+            //legs[i].position = localTargetPos[i]
+            /*
             if (Physics.Raycast( transform.TransformPoint(defaultLocalPos[i]) + transform.up , -transform.up, out RaycastHit hit, 10f))
             {
                 Debug.DrawRay(transform.TransformPoint(defaultLocalPos[i]) + transform.up, -transform.up, Color.green);
                 surfaceNormal = hit.normal;
                 Vector3 hitPoint = hit.point;
                 Vector3 newPos = transform.InverseTransformPoint(hitPoint);
-                
+
                 //raycast in moving direction
                 if (Physics.Raycast(transform.TransformPoint(defaultLocalPos[i]) + transform.up, velocity, out RaycastHit fHit, 1.5f))
                 {
@@ -101,24 +152,24 @@ public class SpiderPCA : MonoBehaviour
                         Debug.DrawRay(transform.TransformPoint(defaultLocalPos[i]) + transform.up, -surfaceNormal, Color.white);
                         newPos = transform.InverseTransformPoint(hitPoint);
                     }
-                    
+
                 }
                 else{
-                    
+
                 }
-                
-                
-                
-                
+
+
+
+
                 //stop target getting too far away due to raycast
                 if ((defaultLocalPos[i] - newPos).magnitude < maxTargetSurfaceOffset)
                 {
-                
+
                     localTargetPos[i] = transform.InverseTransformPoint(hitPoint);
                //}
                 }
-                
-                
+
+
             }
             //on the edge of a plane
             else{
@@ -130,7 +181,7 @@ public class SpiderPCA : MonoBehaviour
                     //Debug.DrawRay(edgeVec, -velocity, Color.green);
                     //normal = ehit.normal;
                     Vector3 newPos = transform.InverseTransformPoint(ehit.point);
-                    
+
                     //stop target getting too far away due to raycast
                     if ((defaultLocalPos[i] - newPos).magnitude < maxTargetSurfaceOffset)
                     {
@@ -142,9 +193,18 @@ public class SpiderPCA : MonoBehaviour
                 else{
                     Debug.DrawRay(edgeVec, -velocity, Color.red);
                 }
-                
+
             }
-            
+            */
+           
+            /*
+            Vector3 LegTargetToAboveSpider = (transform.position + (transform.up * 1.5f)) - transform.TransformPoint(legs[i].position);
+            Collider[] hitColliders = Physics.OverlapSphere(legs[i].position + (LegTargetToAboveSpider * 0.2f), 0f);
+            if (hitColliders.Length > 0)
+            {
+                legs[i].position = transform.TransformPoint(localTargetPos[i]);
+            }*/
+
             //get distance between current leg pos and target point
             if ( (legs[i].position - transform.TransformPoint(localTargetPos[i])).magnitude > maxDistanceFromTarget)
             {
